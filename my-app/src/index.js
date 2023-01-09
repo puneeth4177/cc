e971357409d667d085e47c9cfb3acc655fe9c6e0
puneeth4177 <71485061+puneeth4177@users.noreply.github.com>
2023-01-09 13:05:23 +0530

    Add files via upload

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { AppError } from './components/AppError';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppError>
+      <App />
+    </AppError>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
